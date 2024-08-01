9d475a886146b840c79af1be01b08ad4d8ccf573
Samuel Shemtov <samuel.shemtov@epitech.eu>
2024-08-01 11:43:20 +0200

    add: init

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import json
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+
+
+load_dotenv()
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+OPENAI_KEY = os.getenv('OPENAI_KEY')
+AUTHORIZED_TABLES_NAMES_STR = os.getenv('AUTHORIZED_TABLES_NAMES')
+AUTHORIZED_TABLES_NAMES = AUTHORIZED_TABLES_NAMES_STR.split(",")
+
+
+
+# supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# response = supabase.table("product_categories").select("*").execute()
+# print(json.dumps(response.data, indent=4))
+
+print(AUTHORIZED_TABLES_NAMES)
